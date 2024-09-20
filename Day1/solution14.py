8a15b78e2f9a69a18cf6339d526615d790dadceb
Surya Teja <s.suryateja451@gmail.com>
2024-09-20 19:21:42 +0200

    Adding 14th problem and solution

---
+++ Day1/solution14.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+
+def findHeavyAnimals(animals: pd.DataFrame) -> pd.DataFrame:
+    df = animals[animals["weight"] > 100].sort_values(["weight"], ascending=False)[
+        ["name"]
+    ]
+    return df
