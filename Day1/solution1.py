834ad58cb2f5c7bda24754cd952601a30edf57fb
Surya Teja <s.suryateja451@gmail.com>
2024-09-20 19:36:23 +0200

    Corrected solution1

--- Day1/solution1.py
+++ Day1/solution1.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
 
-def selectData(students: pd.DataFrame) -> pd.DataFrame:
-    df = students.query("student_id==101")
-    return df[["name", "age"]]
+def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
+    df = pd.DataFrame(student_data)
+    df.rename(columns={0: "student_id"}, inplace=True)
+    df.rename(columns={1: "age"}, inplace=True)
+    return df
--- Day1/solution2.py
+++ Day1/solution2.py
@@ -1,8 +1,1 @@
 import pandas as pd
-
-
-def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
-    df = pd.DataFrame(student_data)
-    df.rename(columns={0: "student_id"}, inplace=True)
-    df.rename(columns={1: "age"}, inplace=True)
-    return df
